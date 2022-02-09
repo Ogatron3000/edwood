9797e2ecf9c5d99b97b3b3b402066613e934c8dc
Ivan Mijanovic <ivanmijanovic18@gmail.com>
2022-02-09 13:13:28 +0100

    updated styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer>
             <div className="container">
-                <span>EDWD</span> stands for <a href="https://en.wikipedia.org/wiki/Ed_Wood">Ed Wood</a>,
+                <span>EDWD</span> stands for <a href="https://en.wikipedia.org/wiki/Ed_Wood" target="_blank">Ed Wood</a>,
                 an American filmmaker, actor, and author.
             </div>
         </footer>
