6c9720c68b52499ed9a8fd65e19b0d7e309cc273
Ivan Mijanovic <ivanmijanovic18@gmail.com>
2022-02-09 13:05:10 +0100

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import './Footer.css';
+
+export default function Footer() {
+    return (
+        <footer>
+            <div className="container">
+                <span>EDWD</span> stands for <a href="https://en.wikipedia.org/wiki/Ed_Wood">Ed Wood</a>,
+                an American filmmaker, actor, and author.
+            </div>
+        </footer>
+    )
+}
