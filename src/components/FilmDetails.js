c001ae9c95202de312e70f29329db18b05d779d5
Ivan Mijanovic <ivanmijanovic18@gmail.com>
2022-02-12 21:11:08 +0100

    film details data fields refactor

--- src/components/FilmDetails.js
+++ src/components/FilmDetails.js
@@ -22,6 +22,12 @@ export default function FilmDetails() {
         setActiveTab(e.target.id);
     }
 
+    let releaseYear, director;
+    if (film) {
+        releaseYear = film.release_date && film.release_date.split('-')[0];
+        director = film.credits.crew.find(c => c.job === 'Director');
+    }
+
     return (
         <>
             {film &&
@@ -40,32 +46,50 @@ export default function FilmDetails() {
                                 <div className="film__top container" style={film.backdrop_path ? null : {paddingTop: 'var(--page-padding)'}}>
                                     <div className="film__info">
                                         <h1 className="film__title hide-for-desktop">{film.title}</h1>
-                                        <div className="film__year hide-for-desktop">{film.release_date.split('-')[0]} · Directed by </div>
-                                        <div className="film__director hide-for-desktop">{film.credits.crew.find(c => c.job === 'Director')?.name}</div>
+                                        <div className="film__year hide-for-desktop">
+                                            {releaseYear}
+                                            {director && ' · Directed by'}
+                                        </div>
+                                        <div className="film__director hide-for-desktop">
+                                            {director?.name}
+                                        </div>
                                         <div className="film__rating">
                                             <span>{film.vote_average}</span>
                                             <div className="film__stars" style={{'--rating': film.vote_average}}/>
                                         </div>
                                         <div className="film__trailer-and-duration">
                                             <TrailerButton videos={film.videos.results}/>
-                                            <span>{film.runtime} mins</span>
+                                            <span>
+                                                {film.runtime > 0 && film.runtime + ' mins'}
+                                            </span>
                                         </div>
                                     </div>
                                     <div className="film__poster">
-                                        <img src={`https://image.tmdb.org/t/p/original${film.poster_path}`} alt=""/>
+                                        {film.poster_path && <img src={`https://image.tmdb.org/t/p/original${film.poster_path}`} alt={film.title + ' poster.'}/>}
                                     </div>
                                 </div>
                             </div>
                             <div className="film__bottom container">
                                 <h1 className="film__title show-for-desktop">{film.title}</h1>
                                 <div className="film__below-title show-for-desktop">
-                                    <div className="film__year">{film.release_date.split('-')[0]} · Directed by </div>
-                                    <div className="film__director">{film.credits.crew.find(c => c.job === 'Director')?.name}</div>
+                                    <div className="film__year">
+                                        {releaseYear}
+                                        {director && ' · Directed by'}
+                                    </div>
+                                    <div className="film__director">
+                                        {director?.name}
+                                    </div>
                                 </div>
-                                {film.tagline && <h2 className="film__tagline">{film.tagline}</h2>}
-                                <p className="film__overview">
-                                    {film.overview}
-                                </p>
+                                {film.tagline &&
+                                    <h2 className="film__tagline">
+                                        {film.tagline}
+                                    </h2>
+                                }
+                                {film.overview &&
+                                    <p className="film__overview">
+                                        {film.overview}
+                                    </p>
+                                }
                                 <div className="film__tabs">
                                     <button className="button button-secondary" id='cast' onClick={activateTab}>Cast</button>
                                     <button className="button button-secondary" id='crew' onClick={activateTab}>Crew</button>
--- src/components/ProductionDetails.js
+++ src/components/ProductionDetails.js
@@ -4,22 +4,30 @@ export default function Cast({ film }) {
 
     return (
         <>
-            <div>
-                <h3>Budget</h3>
-                <span>${budget.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</span>
-            </div>
-            <div>
-                <h3>{production_companies.length > 1 ? 'Studios' : 'Studio'}</h3>
-                {production_companies.map(c => <span key={c.id}>{c.name}</span>)}
-            </div>
-            <div>
-                <h3>{production_countries.length > 1 ? 'Countries' : 'Country'}</h3>
-                {production_countries.map(c => <span key={c['iso_3166_1']}>{c['iso_3166_1']}</span>)}
-            </div>
-            <div>
-                <h3>{spoken_languages.length > 1 ? 'Languages' : 'Language'}</h3>
-                {spoken_languages.map(l => <span key={l.english_name}>{l.english_name}</span>)}
-            </div>
+            {budget > 0 &&
+                <div>
+                    <h3>Budget</h3>
+                    <span>${budget.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</span>
+                </div>
+            }
+            {production_companies.length > 0 &&
+                <div>
+                    <h3>{production_companies.length > 1 ? 'Studios' : 'Studio'}</h3>
+                    {production_companies.map(c => <span key={c.id}>{c.name}</span>)}
+                </div>
+            }
+            {production_countries.length > 0 &&
+                <div>
+                    <h3>{production_countries.length > 1 ? 'Countries' : 'Country'}</h3>
+                    {production_countries.map(c => <span key={c['iso_3166_1']}>{c['iso_3166_1']}</span>)}
+                </div>
+            }
+            {spoken_languages.length > 0 &&
+                <div>
+                    <h3>{spoken_languages.length > 1 ? 'Languages' : 'Language'}</h3>
+                    {spoken_languages.map(l => <span key={l.english_name}>{l.english_name}</span>)}
+                </div>
+            }
         </>
     );
 }
